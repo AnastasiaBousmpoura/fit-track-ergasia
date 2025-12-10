404a8679253f1b3412222de2ec6e5f34620e5a82
it2023065 <it2023065@hua.gr>
2025-12-10 15:00:02 +0000

    class trainerAvailability εκκρεμοτητα δεν ελεγχει αν ο trainer ειναι ηδη δε ραντεβου

--- src/main/java/gr/hua/dit/fittrack/core/repository/TrainerAvailabilityRepository.java
+++ src/main/java/gr/hua/dit/fittrack/core/repository/TrainerAvailabilityRepository.java
@@ -1,4 +1,13 @@
 package gr.hua.dit.fittrack.core.repository;
 
-public class TrainerAvailabilityRepository {
+import gr.hua.dit.fittrack.core.model.entity.TrainerAvailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TrainerAvailabilityRepository extends JpaRepository<TrainerAvailability, Long> {
+
+    List<TrainerAvailability> findByTrainerId(Long trainerId);
 }
+
+// ekremmothta na elegxei ama o trainer einei hdh se ranteboy
